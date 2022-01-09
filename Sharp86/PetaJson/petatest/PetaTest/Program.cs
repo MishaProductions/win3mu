3564d79df847d9d1b0e9e07f5d38c0839862fbfc
Misha <46088515+MishaTY@users.noreply.github.com>
2022-01-09 07:59:27 -0500

    readd sharp86

---
+++ Sharp86/PetaJson/petatest/PetaTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PetaTest
+{
+	class Program
+	{
+		static int Main(string[] args)
+		{
+			Runner.RunMain(args);
+			return 0;
+		}
+	}
+}
